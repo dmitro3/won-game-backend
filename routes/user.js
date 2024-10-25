417b9188b22373aedf9c7e6ee70d605ab50c418f
woon <sherman@mobilehead.com>
2024-10-24 20:17:13 -0600

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,7 @@ const ctrl = require('../controllers/user');
 router.post('/:id', ctrl.userData);
 router.put('/:id', ctrl.updateUser);
 router.put('/token/:id', ctrl.updateToken);
+router.put('/withActivity/:id', ctrl.updateUserWithActivity);
 router.get('/ranking', ctrl.ranking);
 
 module.exports = router;
